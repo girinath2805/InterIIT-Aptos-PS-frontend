f6b6c9d904215f0e279e3577dfba29ed5b19d390
Girinath <giri28may@gmail.com>
2023-11-28 01:55:21 +0530

    navbarupdate1

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export {default as History} from './History'
 export {default as Stream} from './Stream'
+export {default as Navbar} from './Navbar'
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
+    
   },
   plugins: [],
 }
